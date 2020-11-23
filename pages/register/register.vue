519c8ca1c849586a83e786e63adb1af4c27a789b
yangwl <1586569655@qq.com>
2020-11-23 14:02:35 +0800

    token

--- pages/register/register.vue
+++ pages/register/register.vue
@@ -8,22 +8,21 @@
 
 		<view class="form">
 			<view class="inputWrapper">
-				<input name="username" class="input" type="text" @input="nameInput" value="" placeholder="请输入用户名">
+				<input name="Username" class="input" type="text" @input="nameInput" value="" placeholder="请输入用户名">
 			</view>
 			<view class="inputWrapper">
-				<input name="password" class="input" type="password" @input="wordInput" value="" placeholder="请输入密码">
+				<input name="Password" class="input" type="password" @input="wordInput" value="" placeholder="请输入密码">
 			</view>
-			<view class="uni-list-cell-left" style="color:#F1F1F1">
-				选择界面号:
+			<view class="number">
+				<view class="uni-list-cell-left" style="color:#F1F1F1">
+					选择界面号:
+					<view class="uni-list-cell-db" style="color:#F1F1F1">
+						<picker @change="bindPickerChange" :value="index" :range="array">
+							<view class="uni-input">{{array[index]}}</view>
+						</picker>
+					</view>
+				</view>
 			</view>
-			<view class="uni-list-cell-db" style="color:#F1F1F1">
-				<picker @change="bindPickerChange" :value="index" :range="array">
-					<view class="uni-input">{{array[index]}}</view>
-				</picker>
-			</view>
-			<!-- <view class="inputWrapper">
-				<input class="input" type="jiemian" @input="onKeyInput" value="" placeholder="请输入界面号">
-			</view> -->
 			<button class="button" @click="login">登录</button>
 		</view>
 	</view>
@@ -36,8 +35,8 @@
 				inputValue: '',
 				array: ['7-1', '7-2', '7-3', '7-4', '7-5'],
 				index: 0,
-				username:'',
-				password:''
+				username: '',
+				password: ''
 			}
 		},
 		methods: {
@@ -47,33 +46,46 @@
 			},
 			nameInput: function(event) {
 				console.log("账号输出的是：", event.target.value)
-				this.username = event.target.value
+				this.Username = event.target.value
 			},
 			wordInput: function(event) {
 				console.log("密码输出的是：", event.target.value)
-				this.password = event.target.value
+				this.Password = event.target.value
 			},
 			login: function() {
-				console.log("账号输出的是1：", this.username)
-				console.log("密码输出的是2：", this.password)
-				
+				console.log("账号输出的是1：", this.Username)
+				console.log("密码输出的是2：", this.Password)
+
 				uni.request({
 					url: 'http://192.168.122.200:8890/api/login',
 					method: 'POST',
 					data: {
-						username: this.username,
-						password: this.password
+						username: this.Username,
+						password: this.Password
 					},
 					header: {
-								"Content-Type": "multipart/form-data",
-							},
+						"Content-Type": 'application/x-www-form-urlencoded',
+					},
 					success: res => {
-						console.log("1111",res)
-						if (res.statusCode == 202) {
+						console.log("1111", res)
+						if (res.statusCode == 200) {
 							uni.showToast({
 								icon: 'none',
 								title: '登录成功',
 							});
+							uni.setStorage({
+								key: 'Authorization',
+								data: res.data.msg,
+								success: function() {
+									console.log("存储用户名到本地成功！", res.data.msg);
+									uni.getStorage({
+										key: 'Authorization',
+										success: function(res) {
+											console.log("获取缓存中的Authorization是:",res.data);
+										}
+									});
+								}
+							})
 							console.log('用户选择的界面号是:', this.index)
 							if (this.index == "0") {
 								uni.navigateTo({
@@ -89,13 +101,18 @@
 									title: '请选择正确的界面号',
 								});
 							}
+						} else {
+							uni.showToast({
+								icon: 'none',
+								title: res.data.msg,
+							});
 						}
 						/* uni.showToast({
 							icon: 'none',
 							title: '密码或用户名错误',
 						}); */
-						
-					
+
+
 					},
 				})
 
@@ -122,60 +139,73 @@
 	}
 
 	.avater {
-		width: 300upx;
-		height: 300upx;
+		width: 300rpx;
+		height: 300rpx;
 		overflow: hidden;
 	}
 
 	.avater .img {
 		width: 100%;
-		margin-top: 60px;
+		margin-top: 100rpx;
 
 	}
 
 	.form {
-		padding: 0 100upx;
-		margin-top: 80px;
-
+		padding: 0 100rpx;
+		margin-top: 80rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
 	}
 
 	.inputWrapper {
-		width: 100%;
-		height: 80upx;
+		width: 500rpx;
+		height: 80rpx;
 		background: white;
-		border-radius: 10px;
+		border-radius: 15rpx;
 		box-sizing: border-box;
-		padding: 0 20px;
-		margin-top: 18px;
+		padding: 0 20rpx;
+		margin-top: 36rpx;
 	}
 
 	.inputWrapper .input {
 		width: 100%;
 		height: 100%;
 		text-align: center;
-		font-size: 15px;
+		font-size: 30rpx;
 
 	}
 
 	.button {
 		color: #fff;
-		width: 100%;
-		height: 80upx;
+		width: 500rpx;
+		height: 80rpx;
 		background-color: #77B3D7;
-		border-radius: 10px;
-		margin-top: 20px;
+		border-radius: 15rpx;
+		margin-top: 40rpx;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		flex-direction: column;
+	}
 
+	.number {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 
 	.uni-list-cell-left {
-		margin-top: 20px;
+		margin-top: 40rpx;
+		margin-left: -250rpx;
+
 	}
 
 	.uni-input {
-		margin-top: -25px;
-		margin-left: 100px;
+		margin-top: -40rpx;
+		margin-left: 200rpx;
+		font-size: 30rpx;
+
 	}
 </style>
